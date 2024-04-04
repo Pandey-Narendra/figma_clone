c8bc9e149e8a2393febac5c0e713315deea31fe5
Pandey-Narendra <pandeynarendra.180180107033@gmail.com>
2024-04-05 00:25:26 +0530

    clone set up

---
+++ app/App.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Live from "@/components/Live";
+// import { Room } from "./Room";
+// import { CollaborativeApp } from "./CollaborativeApp";
+
+export default function Page() {
+  return (
+    <div>
+      <Live />
+    </div>
+
+  );
+}
