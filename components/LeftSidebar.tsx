ea7257ce3c263db48566795d62eda6528d9fae42
Pandey-Narendra <pandeynarendra.180180107033@gmail.com>
2024-04-06 21:34:40 +0530

    liveblocks implemented for different users and frabic js

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -10,6 +10,7 @@ const LeftSidebar = ({ allShapes }: { allShapes: Array<any> }) => {
     const memoizedShapes = useMemo(
         () => (
             <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
+              
                 <div className='text-white'>
                     <h1>1 On Development Mode</h1>
                     <p>2 Open the application on two tabs and try to draw the shapes to implement liveblocks</p>
